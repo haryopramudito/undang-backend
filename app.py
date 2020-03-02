4af6e2068dcf254b56bb3920731b0fb2e57a5eb7
Haryo Pramudito <haryo_pramudito@yahoo.com.au>
2020-03-02 16:04:34 +0000

    first initiate

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask
+from flask_cors import CORS
+
+# use flask library
+app = Flask(__name__)
+CORS(app)
+
+# index URL Route
+@app.route('/')
+def index():
+    return jsonify({"message": "OK: Hello World"}), 200
+
+# Call Main Program
+if __name__ == '__main__':
+    app.run(debug=False, host='0.0.0.0',port=8080)	
