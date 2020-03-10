a35fdee97c66b2fae57c6be0dedeb33d088fcd6e
U-LAPTOP-V9E0P8MS\HaryoPramudito <haryopramudito545@ocft.com>
2020-03-10 11:02:55 +0700

    ubah connection string mongodb, dengan auth

--- config/mongo_db.py
+++ config/mongo_db.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask_pymongo import PyMongo
+import urllib.parse
 
 apps = Flask(__name__)
-apps.config["MONGO_URI"] = "mongodb://localhost:27017/undang"
+apps.config["MONGO_URI"] = "mongodb://undang:"+urllib.parse.quote("undang@2020")+"@localhost:27017/undang"
 mongo = PyMongo(apps)
